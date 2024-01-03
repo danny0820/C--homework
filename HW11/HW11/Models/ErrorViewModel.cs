7dff90166bcaafdd0c938e2aff75672806c88b1b
danny <ccc3690755@gmail.com>
2024-01-03 10:35:21 +0800

    HW11

---
+++ HW11/HW11/Models/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+namespace HW11.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+
+}
